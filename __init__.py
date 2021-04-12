cc800de6b6e61178775b7c104e56ede97f9bd604
Alessandro Aldegheri <alealde012@gmail.com>
2021-04-12 12:01:46 +0200

    MechSite - Version 0.2 - 12/04/2021 Aggiunti: -database completo (tabelle, relazioni) -completata pagina "aggiuntamezzi" -elaborazione parziale pagina "storico", funzionamento + query -debugging nomi classi/tabelle database -er + logico completati

--- __init__.py
+++ __init__.py
@@ -1,20 +1,21 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+import time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class clienti(db.Model):
+class Clienti(db.Model):
     cf = db.Column("cf", db.String(16), primary_key=True)
     nome = db.Column("nome", db.String(30))
     cognome = db.Column("cognome", db.String(30))
     ntelefono = db.Column("ntelefono", db.String(10))
     email = db.Column("email", db.String(100))
     password = db.Column("password", db.String(20))
-
+    mecchina = db.relationship("Mezzi", backref="clienti", lazy=True)
     def __init__(self, cf,  nome, cognome, ntelefono, email, password):
         self.cf = cf
         self.nome = nome
@@ -22,6 +23,40 @@ class clienti(db.Model):
         self.ntelefono = ntelefono
         self.email = email
         self.password = password
+
+class Mezzi(db.Model):
+    targa = db.Column("targa", db.String(10), primary_key=True)
+    marca = db.Column("marca", db.String(30))
+    modello = db.Column("modello", db.String(30))
+    cilindrata = db.Column("cilindrata", db.String(10))
+    potenza = db.Column("potenza", db.String(10))
+    cfcliente = db.Column("cfcliente", db.String(20), db.ForeignKey('clienti.cf'), nullable=False)
+    riparazione = db.relationship("Riparazioni", backref="mezzi", lazy=True)
+
+    def __init__(self, targa, marca, modello, cilindrata, potenza, cfcliente):
+        self.targa = targa
+        self.marca = marca
+        self.modello = modello
+        self.cilindrata = cilindrata
+        self.potenza = potenza
+        self.cfcliente = cfcliente
+
+class Riparazioni(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    stato = db.Column("stato", db.Boolean, default=False)
+    inizio = db.Column("inizio", db.String(30))
+    fine = db.Column("fine", db.String(30))
+    prezzo = db.Column("prezzo", db.String(10))
+    descrizione = db.Column("descrizione", db.String(50))
+    targamezzo = db.Column("targamezzo", db.String(20), db.ForeignKey('mezzi.targa'), nullable=False)
+
+    def __init__(self, stato, inizio, fine, prezzo, descrizione, targamezzo):
+        self.stato = stato
+        self.inizio = inizio
+        self.fine = fine
+        self.prezzo = prezzo
+        self.descrizione = descrizione
+        self.targamezzo = targamezzo
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -47,10 +82,23 @@ def riparazioni():
     else:
         return redirect(url_for("home"))
 
-@app.route("/storico")
+@app.route("/storico", methods=["GET", "POST"])
 def storico():
     if "pass" in session:
-        return render_template("riparazioni.html")
+        if request.method=="POST":
+            scelta = request.form["scelta"]
+            if scelta=="CF":
+                cf = request.form["ricerca"]
+                lista=Mezzi.query.filter_by(cfcliente=cf).all()
+                return render_template("storico.html", controllo=1, listamacchine=lista)
+            elif scelta=="Targa":
+                targain=request.form["ricerca"]
+                lista=Clienti.query.join(Mezzi, Clienti.cf==Mezzi.cfcliente).filter_by(targa=targain).all()
+                return render_template("storico.html", controllo=2, listaproprietari=lista)
+        else:
+            lista1 = Clienti.query.all()
+            lista2 = Mezzi.query.all()
+            return render_template("storico.html", controllo=0, listamezzi=lista2, listaclienti=lista1)
     else:
         return redirect(url_for("home"))
 
@@ -73,11 +121,11 @@ def aggiuntaclienti():
             ntelefono = request.form["ntelefono"]
             email = request.form["email"]
             password = None
-            check_cliente = clienti.query.filter_by(cf=cf).first()
+            check_cliente = Clienti.query.filter_by(cf=cf).first()
             if check_cliente:
                 return render_template("addclienti.html", frase="cf già inserito")
             else:
-                cliente = clienti(cf, nome, cognome, ntelefono, email, password)
+                cliente = Clienti(cf, nome, cognome, ntelefono, email, password)
                 db.session.add(cliente)
                 db.session.commit()
                 return render_template("addclienti.html", frase="aggiunto")
@@ -87,10 +135,28 @@ def aggiuntaclienti():
         return redirect(url_for("home"))
 
 
-@app.route("/aggiuntamezzi")
+@app.route("/aggiuntamezzi", methods=["GET", "POST"])
 def aggiuntamezzi():
     if "pass" in session:
-        return render_template("riparazioni.html")
+        listaclienti = Clienti.query.all()
+        if request.method == "POST":
+            targa = request.form["targa"]
+            marca = request.form["marca"]
+            modello = request.form["modello"]
+            cilindrata = request.form["cilindrata"]
+            potenza = request.form["potenza"]
+            cfcliente = request.form["cfcliente"]
+            check_mezzo = Mezzi.query.filter_by(targa=targa).first()
+            if check_mezzo:
+                return render_template("addmezzi.html", lista=listaclienti, frase="targa già inserita")
+            else:
+                mezzo = Mezzi(targa, marca, modello, cilindrata, potenza, cfcliente)
+                db.session.add(mezzo)
+                db.session.commit()
+                return render_template("addmezzi.html", lista=listaclienti, frase="aggiunto")
+        else:
+
+            return render_template("addmezzi.html", lista=listaclienti)
     else:
         return redirect(url_for("home"))
 
